af0ca5f6a1cbef96cac8200550cf1b836e059ed3
Seyed Mohammadjavad Seyed Talebi <mjavad@uci.edu>
2020-02-20 17:20:27 -0800

    sock.h added

---
+++ SampleApp/src/sock.h
@@ -0,0 +1,9 @@
+#define PortNumber      9876
+#define PortNumber_stop      9878
+#define PortNumber_start      9880
+#define PortNumber_MegaMindEngine      9882
+#define MaxConnects        8
+#define BuffSize         256
+#define ConversationLen    3
+#define Host            "localhost"
+
